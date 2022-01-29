2be6994b3ddd5c04a26d237525a203edde17d210
Vasilis Desktop <vamaragos@gmail.com>
2022-01-29 19:19:18 +0200

    Created a new check for store and update methods

--- app/Http/Controllers/VoyagesAPIController.php
+++ app/Http/Controllers/VoyagesAPIController.php
@@ -13,25 +13,29 @@ class VoyagesAPIController extends Controller
     {
         request()->validate([
             'vessel_id' => ['required'],
-            // 'code' => ['string', 'unique:voyages,code'],
             'start' => ['required', 'date'],
             'end' => ['date', 'after:start'],
-            // 'status' => [
-            //     'required', 
-            //     'string',
-            //     Rule::in(['pending', 'ongoing', 'submitted'])
-            // ],
             'revenues' => ['numeric'],
             'expenses' => ['numeric'],
         ]);
 
         $vessel = Vessel::findOrFail(request('vessel_id'));
         $vessel_name = $vessel->name;
+        $voyage_code = $vessel_name . '-' . request('start');
+
+        foreach ($vessel->voyages as $voyage){
+            if($voyage->code == $voyage_code){
+                return [
+                    'error' => 'A voyage with the same vessel and start date already exist.'
+                ];
+                break;
+            }
+        }
 
         return Voyage::create([
 
             'vessel_id' => request('vessel_id'),
-            'code' => $vessel_name . '-' . request('start'),
+            'code' => $voyage_code,
             'start' => request('start'),
             'end' => request('end'),
             // 'status' => 'pending',
@@ -56,6 +60,7 @@ class VoyagesAPIController extends Controller
             'expenses' => ['numeric'],
         ]);
         
+        // check if voyage has already been submitted
         if ($voyage->status == 'submitted'){
             return [
                 'error' => 'This voyage has been submitted and can no longer be updated.'
@@ -64,7 +69,8 @@ class VoyagesAPIController extends Controller
         
         $vessel = $voyage->vessel;
         $voyages = $vessel->voyages;
-        
+
+        // check if voyage status is ongoing and vessel already has an ongoing voyage
         if (request('status') == 'ongoing'){
             foreach ($voyages as $voyage)
             {
@@ -79,9 +85,18 @@ class VoyagesAPIController extends Controller
         }
         
         $vessel_name = $vessel->name;
+        $voyage_code = $vessel_name . '-' . request('start');
 
-        // ddd($vessel_name);
-        
+        // check if a voyage with the same start date and vessel already exists
+        foreach ($vessel->voyages as $voyage){
+            if($voyage->code == $voyage_code){
+                return [
+                    'error' => 'A voyage with the same vessel and start date already exist.'
+                ];
+                break;
+            }
+        }
+
         $voyage->update([
             'start'  => request('start'),
             'end'  => request('end'),
