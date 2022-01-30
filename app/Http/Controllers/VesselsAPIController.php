c0b80c4ac877ba393d4aca84af5a726f06538ef9
Vasilis Desktop <vamaragos@gmail.com>
2022-01-30 22:04:58 +0200

    financial-report/GET endpoint now returns the vessel's average daily profit

--- app/Http/Controllers/VesselsAPIController.php
+++ app/Http/Controllers/VesselsAPIController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Vessel;
 use Carbon\Carbon;
+use ErrorException;
+use Exception;
 use Illuminate\Http\Request;
 
 class VesselsAPIController extends Controller
@@ -11,13 +13,27 @@ class VesselsAPIController extends Controller
     public function show(Vessel $vessel)
     {
         $voyages = $vessel->voyages;
-        foreach ($voyages as $voyage){
-            $start = $voyage->created_at;
-            $end = $voyage->updated_at;
-            ddd($start, $end);
-            $voyage_duration = $end->diffInDays(Carbon::parse($start));
-            ddd($voyage_duration);
-            $daily_average = $voyage->profit / $voyage_duration;
+        
+        try{
+            $voyage_total_days = null;
+            $voyage_total_profits = null;
+            foreach ($voyages as $voyage){
+                
+                $start = Carbon::parse($voyage->start);
+                $end = Carbon::parse($voyage->end);
+                $voyage_total_days += $start->diffInDays($end);
+                $voyage_total_profits += $voyage->profit;
+            }
+            $voyage_avg_daily_profit = $voyage_total_profits / $voyage_total_days;
+            return [
+                'daily_profit_avg' => $voyage_avg_daily_profit
+            ];
+        }
+        catch(Exception $e)
+        {
+            return [
+                'error' => 'This vessel has no voyages yet'
+            ];
         }
     }
 
